5cf77878c9010238f4ddddfa2fecb0f3723442b1
Joshua Hoffman <joshbhoffman@gmail.com>
2024-08-20 16:49:26 -0500

    Ready to test and deploy

--- attendancetracker/tracker.py
+++ attendancetracker/tracker.py
@@ -1,13 +1,16 @@
 import random
 import pickle
 import sys
+import atexit
 from colorama import Fore, Style
+
+RECORDS_FNAME = 'records.pkl'
 
 def add_record(records: dict, student: str) -> None:
     ''' Add a record to the records. '''
     records[student] = []
 
-def load_records(file: str='records.pkl') -> dict:
+def load_records(file: str=RECORDS_FNAME) -> dict:
     ''' Load the records from the file. '''
     try:
         with open(file, 'rb') as file:
@@ -17,7 +20,7 @@ def load_records(file: str='records.pkl') -> dict:
         print(Style.RESET_ALL)
         return {}
 
-def save_records(records: dict, file: str='records.pkl') -> None:
+def save_records(records: dict, file: str=RECORDS_FNAME) -> None:
     ''' Save the records to the file. '''
     try:
         with open(file, 'wb') as file:
@@ -26,11 +29,6 @@ def save_records(records: dict, file: str='records.pkl') -> None:
         print(Fore.RED + 'IT WAS NOT SAVED CORRECTLY!')
         print(Style.RESET_ALL)
         print(e)
-
-def get_weights(students: dict) -> list:
-    """Get the weights of each student."""
-    total = sum(students.values())
-    return [students[student] / total for student in students.keys()]
 
 def pick_someone(students: dict) -> str:
     """Pick a random student from the list of students."""
@@ -47,12 +45,14 @@ def update_attendance(students: dict, student: str, response: str="noop") -> boo
     elif response == "absent":
         students[student].append(0)
         return True
+    elif response == "excused":
+        return True
     else:
         return False
 
 def question_response(students: dict, student: str) -> None:
     """Loop for the interaction."""
-    valid_options = ["answered", "passed", "absent", "exit"]
+    valid_options = ["answered", "passed", "absent", "excused", "exit"]
     valid_response = False
     # TODO: Test this loop and all possible paths
     while not valid_response:
@@ -68,18 +68,18 @@ def question_response(students: dict, student: str) -> None:
         if not valid_response:
             print(Fore.YELLOW + "Invalid response. Please try again." + Style.RESET_ALL + "\n")
 
-def death_knell(students: dict, file: str='records.pkl') -> None:
+def death_knell(students: dict, file: str=RECORDS_FNAME) -> None:
     """Print a message to the user."""
     save_records(records=students, file=file)
 
 
-def user_loop() -> None:
+def user_loop(student_records: dict) -> None:
     """Loop for the user interaction."""
     # TODO: see scratch for how to ensure save on atexit
-    student_records = load_records()
     # TODO: add a way to add a student
     while True:
-        picked = input("Type in \'next\' to continue or \'exit\'.\n? ")
+        picked = input("Type in \'next\' to continue, \'add\' to add a student, or \'exit\'.\n? ")
+        picked = picked.lower()
         if picked == 'next':
             student = pick_someone(student_records)
             print(f"The student is {student}.")
@@ -88,14 +88,27 @@ def user_loop() -> None:
             death_knell(student_records)
             print(Fore.GREEN + "The records have been saved." + Style.RESET_ALL)
             exit()
+        elif picked == 'add':
+            student = input("Type in the student's name: ")
+            add_record(student_records, student)
         else:
             print(Fore.YELLOW + "Invalid response. Please try again." + Style.RESET_ALL + "\n")
 
+def lifecycle_loop() -> None:
+    """The main loop for the program."""
+    record_name = RECORDS_FNAME
+    input_name = input("Type in the file name to load in the records: ")
+    if input_name != '':
+        record_name = input_name
+
+    records = load_records(record_name)
+
+    # death_knell(students: dict, file: str=RECORDS_FNAME)
+    atexit.register(death_knell, students=records, file=record_name)
+
+    # Run the loop using the loaded in records
+    user_loop(records)
+
 if __name__ == "__main__":
-    student_data = {
-        "Alice": 1000000000,
-        "Bob": 20,
-        "Charlie": 30,
-        "David": 40,
-    }
-    print(pick_someone(student_data))
+    # Call the main function 
+    lifecycle_loop()
